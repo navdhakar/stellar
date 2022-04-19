3d94296f9dea5df25798c83ea8a0424b9b482753
navdeep <navdepdhakar1@gmail.com>
2022-04-19 22:29:45 +0530

    sdfdSF

--- src/configure.js
+++ src/configure.js
@@ -1,1 +1,1 @@
-export const server = process.env.NODE_ENV == "production" ? "https://nebulaengine.herokuapp.com/" : "http://127.0.0.1:5000";
+export const server = process.env.NODE_ENV == "production" ? "https://nebulaengine.herokuapp.com" : "http://127.0.0.1:5000";
